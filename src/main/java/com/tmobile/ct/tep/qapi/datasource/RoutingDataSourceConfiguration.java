23e8112fbbfa5abf4c0e5fc0aad29a640b7b5f7d
Julio Zevallos <juliozev@gmail.com>
2019-10-17 10:30:53 -0700

    updated license headers and license file

--- src/main/java/com/tmobile/ct/tep/qapi/datasource/RoutingDataSourceConfiguration.java
+++ src/main/java/com/tmobile/ct/tep/qapi/datasource/RoutingDataSourceConfiguration.java
@@ -1,20 +1,20 @@
 /*
- * // =========================================================================
- * // Copyright © 2019 T-Mobile USA, Inc.
- * //
- * // Licensed under the Apache License, Version 2.0 (the "License");
- * // you may not use this file except in compliance with the License.
- * // You may obtain a copy of the License at
- * //
- * //    http://www.apache.org/licenses/LICENSE-2.0
- * //
- * // Unless required by applicable law or agreed to in writing, software
- * // distributed under the License is distributed on an "AS IS" BASIS,
- * // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * // See the License for the specific language governing permissions and
- * // limitations under the License.
- * // =========================================================================
- *
+ * =========================================================================
+ * Copyright 2019 T-Mobile USA, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * See the LICENSE file for additional language around disclaimer of warranties.
+ * Trademark Disclaimer: Neither the name of “T-Mobile, USA” nor the names of
+ * its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ * ===========================================================================
  */
 
 package com.tmobile.ct.tep.qapi.datasource;
@@ -125,8 +125,8 @@ public class RoutingDataSourceConfiguration {
             scanner = new Scanner(fileInputStream);
             HashMap<String, String> secrets = new HashMap<>();
             while (scanner.hasNextLine()) {
-                String value[] = scanner.nextLine().split("=");
-                secrets.put(value[0], value[1]);
+                String value[] = scanner.nextLine().split("=",2);
+                secrets.put(value[0].trim(), value[1].trim());
             }
             return secrets;
         } finally {
